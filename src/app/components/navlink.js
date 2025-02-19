a6c4ffd9927f22425f4fab9c1077880646a30926
SoftLifeDev <softlife.dev001@gmail.com>
2025-02-19 11:00:39 +0100

    Update navlink.js

--- src/app/components/navlink.js
+++ src/app/components/navlink.js
@@ -8,7 +8,7 @@ const NavLink = ({ link }) => {
 
   return (
     <Link className={pathName === link.url ? "active" : ""} href={link.url}>
-     <span> {link.icon}</span> <span id='title'>{link.title}</span>
+     <span> {link.icon}</span>{link.title}
     </Link>
   );
 };
