6d5a0a6ee27ceea32f86c68908f9f65485f2336b
SoftLifeDev <softlife.dev001@gmail.com>
2025-02-22 00:28:28 +0100

    Update home1.js

--- src/app/components/home1.js
+++ src/app/components/home1.js
@@ -25,7 +25,7 @@ function Home1() {
             <br />a Front End Developer and <span id="hobby"></span>
           </h2>
           <h1 data-scroll data-scroll-speed={0.1}>
-            Helping brands strive in a digital era.
+            Helping brands strive in the digital era.
           </h1>
           <div className="blob"></div>
           <div data-scroll data-scroll-speed={0.1}>
