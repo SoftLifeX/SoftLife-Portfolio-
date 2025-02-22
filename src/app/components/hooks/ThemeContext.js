bbff33b3f05d82a5b0cad3399ea22b38642de876
SoftLifeDev <softlife.dev001@gmail.com>
2025-02-22 17:53:00 +0100

    Update ThemeContext.jsl

--- src/app/components/hooks/ThemeContext.js
+++ src/app/components/hooks/ThemeContext.js
@@ -7,12 +7,14 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-const [theme, setTheme] = useState(false,  () => localStorage.getItem('theme') || "light");
+const [theme, setTheme] = useState("");
 
   useEffect(() => {
     document.body.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [theme]);
+
+  const prevTheme = localStorage.getItem('theme')
 
  const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
