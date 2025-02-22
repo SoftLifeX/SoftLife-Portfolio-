91a2e3955d066288635829839da20bca6e5c24f7
SoftLifeDev <softlife.dev001@gmail.com>
2025-02-22 22:01:48 +0100

    Update ThemeContext.js

--- src/app/components/hooks/ThemeContext.js
+++ src/app/components/hooks/ThemeContext.js
@@ -12,11 +12,11 @@ export const ThemeProvider = ({ children }) => {
             
   useEffect(() => {
     if(theme === 'light'){
-      document.body.setAttribute('data-theme', theme);
+      document.body.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
     }
     if(theme === 'dark'){
-      document.body.setAttribute('data-theme', theme);
+      document.body.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
     }
     
