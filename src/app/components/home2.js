01503e513be12b7cc70cb62b7890c105669af1ea
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-10 23:00:34 +0100

    Update home2.js

--- src/app/components/home2.js
+++ src/app/components/home2.js
@@ -56,7 +56,7 @@ function Home2() {
               <motion.div 
                initial={{ x: -100 }}
                 whileInView={{ x: 0 }}
-                transition={{ delay: 0.05, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+                transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
               className="work">
                 <Image 
                  priority={true}
@@ -72,7 +72,7 @@ function Home2() {
               <motion.div 
                initial={{ x: -100 }}
                 whileInView={{ x: 0 }}
-                transition={{ delay: 0.1, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+                transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
               className="work">
                 <Image
                  priority={true}
@@ -88,7 +88,7 @@ function Home2() {
               <motion.div 
                initial={{ x: -100 }}
                 whileInView={{ x: 0 }}
-                transition={{ delay: 0.15, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+                transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
               className="work">
                 <Image priority={true} src={images.work1} alt="work1" />
                 <div className="layer">
@@ -102,7 +102,7 @@ function Home2() {
               <motion.div 
                initial={{ x: -100 }}
                 whileInView={{ x: 0 }}
-                transition={{ delay: 0.2, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+                transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
               className="work">
                 <Image priority={true} src={images.work2} alt="work2" />
                 <div className="layer">
