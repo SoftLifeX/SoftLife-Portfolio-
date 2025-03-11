b69ce8c00d0b5898bcf74ed9eb2e518197bfa771
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-11 12:12:24 +0100

    Update home1.js

--- src/app/components/home1.js
+++ src/app/components/home1.js
@@ -13,8 +13,8 @@ function Home1() {
         <div className="homepage">
     <div data-scroll data-scroll-speed={0.1}>
           <motion.h2
-            initial={{ y: "60svh", opacity: 0}}
-            animate={{ y: "0%", opacity: 1}}
+            initial={{ y: 60, opacity: 0}}
+            animate={{ y: 0, opacity: 1}}
             transition={{ delay: 0.3, duration: 1, ease: [0.16, 1, 0.3, 1] }}
           >
             Hello! I'm{" "}
@@ -31,8 +31,8 @@ function Home1() {
             <br />a Front End Developer and <span id="hobby"></span>
           </motion.h2>
           <motion.h1
-            initial={{ y: "60svh" }}
-            animate={{ y: "0%", opacity: 1}}
+            initial={{ y: 60 }}
+            animate={{ y: 0, opacity: 1}}
             transition={{ delay: 0.4, duration: 1, ease: [0.16, 1, 0.3, 1] }} 
           >
             Helping brands thrive in the digital era.
