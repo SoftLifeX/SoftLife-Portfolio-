cc44ef9bf89c6f660dc598ea6a4ea1c26de47b2c
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-12 18:05:05 +0100

    Update about1.js

--- src/app/components/about1.js
+++ src/app/components/about1.js
@@ -8,11 +8,11 @@ function About1() {
     <div>
       <section className="about1">
         <div className="about">
-           <motion.h5 initial={{ opacity: 0, y: 200 }}
+           <motion.h5 initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.85, ease: [0.16, 1, 0.3, 1] }}
          data-scroll data-scroll-speed={0.1}>About</motion.h5>
-          <motion.h1 initial={{ opacity: 0, y: 200 }}
+          <motion.h1 initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.85, ease: [0.16, 1, 0.3, 1] }}
          data-scroll data-scroll-speed={0.1}>
