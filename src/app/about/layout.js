9c312459c25c061459c00c927269ecd0e0e0bfc5
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-13 09:53:09 +0100

    Add files via upload

---
+++ src/app/about/layout.js
@@ -0,0 +1,13 @@
+import Page from "./page"
+
+
+export const metadata = {
+  title: "About me",
+  description: "",
+};
+
+export default function PageLayout({ children }) {
+  return (
+<Page />
+  );
+}
