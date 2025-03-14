a75ace973e20fa1cff2aa73561e0c4263ee4c0fd
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-14 05:01:38 +0100

    Update craft1.js

--- src/app/components/craft1.js
+++ src/app/components/craft1.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image";
 import { images } from "@/app/images/images";
 
 
 function Craft1() {
+
+  const [action,  setAction] = useState("Project");
+  
   return (
     <div>
       <section className="craft1">
@@ -14,7 +17,132 @@ function Craft1() {
             </h1>
 
             <h5>Craft</h5>
+         <div id="stacks">stacks</div>
+         <div className="tab-titles">
+                <h4>
+                  <ul>
+                    <li>
+                      <a href="#stack" className={action === "Project" ? "active": "" }
+                        onClick={() => {
+                        setAction("Project");
+                        }}
+                       >
+                        Featured Projects
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#stack" className={action === "Concept" ? "active": "" }
+                        onClick={() => {
+                        setAction("Concept");
+                        }}
+                       >
+                        Concept Works
+                      </a>
+                    </li>
+                  </ul>
+                </h4>
+              </div>
+
+          {action === "Project" ? (
+            
             <div className="work-list">
+            <h3> A few of my digital crafts </h3>
+              <div className="work">
+                <Image src={images.work2} alt="work2" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work3} alt="work3" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work2} alt="work2" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work1} alt="work1" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+            <p className="fakemargin">margin</p>
+            </div>
+          ) : (
+            <div/>
+            )}
+            {action === "Concept" ? (
+          <div className="work-list">
+            <h3> Concepts in production!</h3>
+              <div className="work">
+                <Image src={images.work2} alt="work2" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work3} alt="work3" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work2} alt="work2" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div className="work">
+                <Image src={images.work1} alt="work1" />
+                <div className="layer">
+                  <p></p>
+                  <a href="">
+                    <i className="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+            <p className="fakemargin">margin</p>
+            </div>
+
+            
+          ) : (
+            <div/>
+            )}
+        </div>
+           /* <div className="work-list">
               <div className="work">
                 <Image src={images.work2} alt="work2" />
                 <div className="layer">
@@ -57,7 +185,7 @@ function Craft1() {
             </div>
 
             <p className="fakemargin">margin</p>
-          </div>
+          </div>*/
         </div>
       </section>
     </div>
