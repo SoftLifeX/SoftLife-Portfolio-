aa820035866e4dfd3262c6d6ed1746d9635055a9
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-15 23:49:49 +0100

    Update useThemeSwitcher.js

--- src/app/components/hooks/useThemeSwitcher.js
+++ src/app/components/hooks/useThemeSwitcher.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const  useThemeSwitcher = () => {
+/*const  useThemeSwitcher = () => {
 
     const preferLightQuery = "(prefer-color-scheme: light)";
     const [mode, setMode] = useState("");
@@ -47,4 +47,56 @@ const  useThemeSwitcher = () => {
   return [mode, setMode]
 }
 
+export default useThemeSwitcher*/
+
+const  useThemeSwitcher = () => {
+
+    const preferLightQuery = "(prefer-color-scheme: light)";
+    const [mode, setMode] = useState("");
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(preferLightQuery);
+        const userPref = window.localStorage.getItem("theme");
+
+        const handleChange = () => {
+            if(userPref){
+                let check = userPref === "light" ? "light" : "dark";
+                setMode(check);
+
+                if(check==="light"){
+                    document.body.setAttribute('data-theme', 'light');
+                }
+                if(check==="dark"){
+                    document.body.setAttribute('data-theme', 'dark');
+                }
+            }else{
+                 let check = mediaQuery.matches ? "light" : "dark";
+                   setMode(check);
+                
+                if(check==="light"){
+                    document.body.setAttribute('data-theme', 'light');
+                }
+                if(check==="dark"){
+                    document.body.setAttribute('data-theme', 'dark');
+                }
+            }
+        }
+        mediaQuery.addEventListener("change", handleChange)
+
+        return () => mediaQuery.removeEventListener("change", handleChange)
+    }, [])
+
+    useEffect(() => {
+        if(mode === "light"){
+            window.localStorage.setItem("theme", "light");
+            document.body.setAttribute('data-theme', 'light');
+        }if(mode === 'dark'){
+             window.localStorage.setItem("theme", "dark");
+            document.body.setAttribute('data-theme', 'dark');
+        }
+    }, [mode])
+  return [mode, setMode]
+}
+
 export default useThemeSwitcher
+            
