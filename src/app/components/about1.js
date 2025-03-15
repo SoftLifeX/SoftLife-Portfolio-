2796f907ad40c0e4c24686e680e75b8bbff878eb
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-15 22:19:03 +0100

    Update about1.js

--- src/app/components/about1.js
+++ src/app/components/about1.js
@@ -11,12 +11,12 @@ function About1() {
         <motion.h5
             initial={{ opacity: 0, y: "60%" }}
                 animate={{ opacity: 1, y: "0%" }}
-            transition={{ delay: 0.8, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+            transition={{ delay: 0.8, duration: 0.5 }}
              data-scroll data-scroll-speed={0.1}>About</motion.h5>
           <motion.h1
             initial={{ opacity: 0, y: "60%" }}
                 animate={{ opacity: 1, y: "0%" }}
-            transition={{ delay: 0.8, duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+            transition={{ delay: 0.8, duration: 0.5 }}
              data-scroll data-scroll-speed={0.1}>
             Creating symphonies of digital functionality and aesthetics
           </motion.h1>
