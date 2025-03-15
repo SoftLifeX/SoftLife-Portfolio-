57c15e480af4259e9f49a6ac63237ed29194ea1d
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-16 00:57:23 +0100

    Update header.js

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -7,7 +7,7 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faFolder } from "@fortawesome/free-solid-svg-icons";
 import useThemeSwitcher from "./hooks/useThemeSwitcher";
-import ThemeProvider from "./hooks/ThemeProvider";
+import ThemeProvider from "./hooks/ThemeContext";
 
 function Header() {
   /*const [mode, setMode] = useThemeSwitcher();*/
