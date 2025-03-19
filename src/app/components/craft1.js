1108c044563fc4f56501ba191d26864c136c54eb
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-19 17:38:37 +0100

    Update craft1.js

--- src/app/components/craft1.js
+++ src/app/components/craft1.js
@@ -45,7 +45,7 @@ function Craft1() {
 
           {action === "Project" ? (
             <div>
-            <h3> My digital crafts </h3>
+            <p> My digital crafts </p>
             
             <div className="work-list">
               <div className="work">
@@ -96,7 +96,7 @@ function Craft1() {
             )}
             {action === "Concept" ? (
           <div>
-            <h3> Concepts in production!</h3>
+            <p> Concepts in production!</p>
           <div className="work-list">
               <div className="work">
                 <Image src={images.work2} alt="work2" />
