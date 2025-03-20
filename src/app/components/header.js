d78f4b5ba48d62ece89593068809fd1ff56b5d73
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-20 17:54:32 +0100

    Update header.js

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -59,6 +59,73 @@ const {theme, toggleTheme} = useTheme();
      ),
    },
  ];
+
+     const raysVariants = {
+    hidden: {
+      strokeOpacity: 0,
+      transition: {
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      },
+    },
+    visible: {
+      strokeOpacity: 1,
+      transition: {
+        staggerChildren: 0.05,
+      },
+    },
+  };
+
+  const rayVariant = {
+    hidden: {
+      pathLength: 0,
+      opacity: 0,
+      // Start from center of the circle
+      scale: 0
+    },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+        // Customize timing for each property
+        pathLength: { duration: 0.3 },
+        opacity: { duration: 0.2 },
+        scale: { duration: 0.3 }
+      }
+    },
+  };
+
+  const shineVariant = {
+    hidden: {
+      opacity: 0,
+      scale: 2,
+      strokeDasharray: "20, 1000",
+      strokeDashoffset: 0,
+      filter: "blur(0px)",
+    },
+    visible: {
+      opacity: [0, 1, 0],
+      strokeDashoffset: [0, -50, -100],
+      filter: ["blur(2px)", "blur(2px)", "blur(0px)"],
+      transition: {
+
+
+        duration: 0.75,
+        ease: 'linear'
+      },
+    },
+
+
+
+  };
+
+  const sunPath =
+    "M70 49.5C70 60.8218 60.8218 70 49.5 70C38.1782 70 29 60.8218 29 49.5C29 38.1782 38.1782 29 49.5 29C60 29 69.5 38 70 49.5Z";
+  const moonPath =
+    "M70 49.5C70 60.8218 60.8218 70 49.5 70C38.1782 70 29 60.8218 29 49.5C29 38.1782 38.1782 29 49.5 29C39 45 49.5 59.5 70 49.5Z"
   return (
     <div>
       <header className="top">
@@ -82,24 +149,55 @@ const {theme, toggleTheme} = useTheme();
         </div>
 
    <div onClick={toggleTheme} id="theme-switch" title='theme'>
-          <svg
+          <m.svg
+            strokeWidth="4"
+            strokeLinecap="round"
+            width={100}
+            height={100}
+            viewBox="0 0 100 100"
+            fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            title="dark mode"
+            className='relative'
           >
-            <path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path>
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            title="light mode"
-          >
-            <path d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759 4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"></path>
-          </svg>
+            <m.path variants={shineVariant} d={moonPath} initial="hidden" animate={theme === 'dark' ? 'visible' : "hidden"} />
+
+
+            <m.g variants={raysVariants} initial='hidden' animate={theme === "light" ? "visible" : "hidden"} style={{ strokeLinecap: 'round' }}>
+              <m.path className="origin-center" variants={rayVariant} d="M50 2V11" />
+              <m.path variants={rayVariant} d="M85 15L78 22" />
+              <m.path variants={rayVariant} d="M98 50H89" />
+              <m.path variants={rayVariant} d="M85 85L78 78" />
+              <m.path variants={rayVariant} d="M50 98V89" />
+              <m.path variants={rayVariant} d="M23 78L16 84" />
+              <m.path variants={rayVariant} d="M11 50H2" />
+              <m.path variants={rayVariant} d="M23 23L16 16" />
+            </m.g>
+
+            <m.path
+              d={sunPath}
+              fill="transparent"
+              transition={{ duration: 1, type: "spring" }}
+              initial={{ fillOpacity: 0, strokeOpacity: 0 }}
+              animate={
+                theme === "dark"
+                  ? {
+                    d: moonPath,
+                    rotate: -360,
+                    scale: 2,
+                    fillOpacity: 0.35,
+                    strokeOpacity: 1,
+                    transition: { delay: 0.1 },
+                  }
+                  : {
+
+                    d: sunPath,
+                    rotate: 0,
+                    fillOpacity: 0.35,
+                    strokeOpacity: 1,
+                  }
+              }
+            />
+          </m.svg>
         </div>
 
         <nav className="navbar">
