37bd4d0bfbaae71863a15cc5053c48b3ade25d11
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-21 12:27:20 +0100

    Update loading.js

--- src/app/loading.js
+++ src/app/loading.js
@@ -32,20 +32,21 @@ export default function loading() {
 
     
     return (
-        <div className="h-screen w-full text-white relative">
-            <div className="w-full h-full flex justify-center flex-col items-center">
-                <h1 className="headertin text-3xl md:text-6xl fixed top-5 left-10">ADG.</h1>
-                <div className="loader">
-                    <p>loading</p>
-                    <div className="words">
-                    <span className="word">Interface</span>
-                    <span className="word">Dataset</span>
-                    <span className="word">Graphics</span>
-                    <span className="word">Aesthet</span>
-                    <span className="word">Operation</span>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <motion.div
+          className="TransitionProvider2"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          {paths[pathName]}
+        </motion.div>
+        <motion.div
+          className="TransitionProvider3"
+          initial={{ height: "200vh" }}
+          animate={{ height: "0vh", transition: { delay: 0.5 } }}
+        />
         </div>
     );
       }
