b54a0f02cb29fcefeecaf3444d7ca9364b3f82d0
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-22 18:46:22 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-/*import { ThemeProvider } from "./components/hooks/ThemeContext";*/
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider } from "./components/hooks/ThemeContext";
 import TransitionProvider from "./components/transitionProvider";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
