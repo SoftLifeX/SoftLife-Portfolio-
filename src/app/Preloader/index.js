c181e1cfa9b31f7493fa19ac071b32bb8660e6e3
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-22 04:10:46 +0100

    Update index.js

--- src/app/Preloader/index.js
+++ src/app/Preloader/index.js
@@ -39,7 +39,7 @@ export default function Index() {
           <motion.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
+            transition={{ duration: 1 }}
             className="loader"
           >
             {words[index]}
