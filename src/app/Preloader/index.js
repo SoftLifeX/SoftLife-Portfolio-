eff504df69a29bfc28d562603c289470be121afd
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-22 03:43:56 +0100

    Add files via upload

---
+++ src/app/Preloader/index.js
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { motion } from "framer-motion";
+
+const words = [
+  "SoftLife",
+  "Designer",
+  "Developer",
+  "Average Noob",
+  "Nice to meet you :)",
+];
+
+export default function Index() {
+  const [index, setIndex] = useState(0);
+
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    setDimension({ width: window.innerWidth, height: window.innerHeight });
+  }, []);
+
+  useEffect(() => {
+    if (index == words.length - 1) return;
+
+    setTimeout(
+      () => {
+        setIndex(index + 1);
+      },
+      index == 0 ? 1000 : 150
+    );
+  }, [index]);
+
+
+  return (
+   <div className="introduction"> {dimension.width > 0 && (
+        <>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="loader"
+          >
+            {words[index]}
+          </motion.p>
+        </>
+      )}
+
+    </div>
+     
+  );
+}
