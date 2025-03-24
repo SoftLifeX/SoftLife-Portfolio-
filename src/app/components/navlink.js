3d1ad94a3351eabe8b22c861689f63c80c971a60
SoftLifeDev <softlife.dev001@gmail.com>
2025-03-24 16:50:10 +0100

    Update navlink.js

--- src/app/components/navlink.js
+++ src/app/components/navlink.js
@@ -8,15 +8,12 @@ import { usePathname } from 'next/navigation'
 
 const NavLink = ({ link }) => {
 
-  const numbers = [1]
   const pathName = usePathname();
 
   return (
-       {numbers.map((num) => (
-    <Link key={num} style={{ "--i": num }} className={pathName === link.url ? "active" : ""} href={link.url}>
+    <Link style={{ "--i": 1 }} className={pathName === link.url ? "active" : ""} href={link.url}>
         {link.title}
   </Link>
-     ))}
   );
 };
 
