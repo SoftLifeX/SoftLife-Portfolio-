a03a7fadd268a3d4373659ecc7e2ca7df1301b4c
SoftLifeDev <softlife.dev001@gmail.com>
2025-04-15 19:02:15 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+/*export const metadata = {
   title: {
     default: "SoftLife Dev",
     template: "%s - SoftLife Dev",
@@ -35,20 +35,83 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
   },
-};
-
-
-/*export const metadata: Metadata = {
-  title: {
-    default: "Softlife Design",
-    template: "%s - Softlife Design",
-  },
-  description: "",
-  keywords: ['Software developer', 'Web Development', 'App Development', 'Front-end', 'Softlife Design', 'award-winning',  'React', 'Next js', 'Motion design'],
-  twitter: {
-    card: "summary_large_image",
-  },
 };*/
+
+export const metadata = {
+  /*icons: {
+    apple: '/apple-icon.png',
+  },*/
+  metadataBase: new URL('https://softlife-dev.vercel.app/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: {
+    default: "SoftLife Dev",
+    template: "%s - SoftLife Dev",
+  },
+  description: "An award-winning, detail-oriented Front-end Software developer specialized in React, Motion design and Next js with expertise in Web Development, ui/ux design, App Development",
+   siteId: '@dev.alexdgreat',
+    creator: 'Ogbuagu Great Alexander',
+    creatorId: '@OgbuaguGreat',
+  //  images: ['https://devalexdgreat.vercel.app/favicon.png'], // Must be an absolute URL
+  },
+  openGraph: {
+    title: {
+    default: "SoftLife Dev",
+    template: "%s - SoftLife Dev",
+  },
+  description: "An award-winning, detail-oriented Front-end Software developer specialized in React, Motion design and Next js with expertise in Web Development, ui/ux design, App Development",
+  url: "https://softlife-dev.vercel.app/",
+    siteName: "SoftLife Dev",
+   /* images: [
+      {
+        url: 'https://devalexdgreat.vercel.app/favicon.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://devalexdgreat.vercel.app/favicon.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Alexander de Great (ADG)',
+      },
+    ], */
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  title: {
+    default: "SoftLife Dev",
+    template: "%s - SoftLife Dev",
+  },
+  description: "An award-winning, detail-oriented Front-end Software developer specialized in React, Motion design and Next js with expertise in Web Development, ui/ux design, App Development",
+  keywords: ['Daniel Daniel Chimbu-okaaomee', 'Daniel Daniel c.', 'Software developer', 'Web Development', 'App Development', 'Front-end', 'Softlife Dev', 'award-winning',  'React', 'Next js', 'Motion design', 'Daniel c. Daniel'],
+  authors: [{ name: 'Daniel Daniel Chimbu-okaaomee' }],
+  creator: 'SoftLife Dev',
+  publisher: 'SoftLife Dev',
+  url: "https://softlife-dev.vercel.app/",
+  //image: "https://devalexdgreat.vercel.app/favicon.png",
+  siteName: "SoftLife Dev",
+  type: "website"
+};
 
 export default function RootLayout({ children }) {
   return (
