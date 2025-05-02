58f8d9079e90173ef81de2fa5f9be46805c5b4de
SoftLifeDev <softlife.dev001@gmail.com>
2025-05-02 05:54:23 +0100

    Update craft1.js

--- src/app/components/craft1.js
+++ src/app/components/craft1.js
@@ -85,7 +85,7 @@ function Craft1() {
               </div>
 
               <div className="work">
-                <Image src={images.work2} 8alt="work2" />
+                <Image src={images.work2} alt="work2" />
                 <div className="layer">
                   <p></p>
                   <a href="">
