4429ad753a53534709c602d2d3674519ae6ff639
SoftLifeDev <softlife.dev001@gmail.com>
2025-05-02 07:05:11 +0100

    Update home1.js

--- src/app/components/home1.js
+++ src/app/components/home1.js
@@ -115,7 +115,7 @@ function Home1() {
                   <span>a Travel Enthusiast ✈️ 🌍</span>
                 </span>
               </motion.h2>
-    <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters" style={{display:"none"}}>
+    <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
       <defs>
       <filter id="marker-shape">
       <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
