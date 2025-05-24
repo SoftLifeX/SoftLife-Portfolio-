fa83a0127127d661b8db30478f28c4d55d028652
SoftLifeDev <softlife.dev001@gmail.com>
2025-05-24 19:47:51 +0100

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -22,11 +22,12 @@ const pathName = usePathname();
     <AnimatePresence mode="wait" >
       <motion.div
         key={pathName}
-        initial={{ y: "20vh", opacity: 0, scale: 0 }}
-        animate={{ y: "0%", opacity: 1, scale: 1 }}
-        exit={{ y: "20vh", opacity: 0, scale: 0 }}
+        initial={{ opacity: 0, scale: 0, transformOrigin: 'top' }}
+        animate={{ opacity: 1, scale: 1, transformOrigin: 'top' }}
+        exit={{ opacity: 0, scale: 0, transformOrigin: 'top'}}
         transition={{ duration: 0.5 }}
       >
+
      
      <Contactform />
 
