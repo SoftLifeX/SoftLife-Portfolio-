68670e0d43647bbba6f70bb3a9dcf8d12ee1664a
SoftLifeDev <softlife.dev001@gmail.com>
2025-05-24 19:23:02 +0100

    Update page.js

--- src/app/craft/page.js
+++ src/app/craft/page.js
@@ -1,22 +1,13 @@
 "use client";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import React from "react";
 import { useEffect } from "react";
 import Craft1 from "../components/craft1";
 import Craft2 from "../components/craft2";
 import Contact from "../components/contact";
 
-/*export const metadata = {
-  title: "My Craft",
-};*/
-
 
 const Craft = () => {
 
-const pathName = usePathname();
-  
      useEffect(() => {
           (async () => {
             const LocomotiveScroll = (await import("locomotive-scroll")).default;
@@ -26,21 +17,13 @@ const pathName = usePathname();
         }, []);
         
   return (
-    <AnimatePresence mode="wait" >
-      <motion.div
-        key={pathName}
-        initial={{ y: "20vh", opacity: 0, scale: 0 }}
-        animate={{ y: "0%", opacity: 1, scale: 1 }}
-        exit={{ y: "20vh", opacity: 0, scale: 0 }}
-        transition={{ duration: 0.5 }}
-      >
+    <div>
 
       <Craft1 />
       <Craft2 />
       <Contact />
 
-    </motion.div>
-</AnimatePresence>
+    </div>
   );
 };
 
