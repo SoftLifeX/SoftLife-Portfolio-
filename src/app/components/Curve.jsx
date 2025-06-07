8d18c54c59174dbf695c4cba60152480c0446913
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-07 18:59:02 +0100

    Create Curve.jsx

---
+++ src/app/components/Curve.jsx
@@ -0,0 +1,103 @@
+'use client';
+import Header from "./header";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import Preloader from "../Preloader"
+import StickyCursor from "../components/stickyCursor"
+import { text, curve, translate } from './anim';
+
+const [isLoading, setIsLoading] = useState(true);
+
+  const paths = {
+
+    "/": "Home",
+
+    "/about": "About",
+
+   "/craft": "Craft",
+
+    "/contact": "Contact"
+
+  }
+
+    useEffect(() =>{
+      setTimeout(() => {
+             setIsLoading(false);
+    
+             document.body.style.cursor = "default";
+    
+             window.scrollTo(0, 0);
+           }, 3000);
+  },[]);
+
+const anim = (variants) => {
+    return {
+        variants,
+        initial: "initial",
+        animate: "enter",
+        exit: "exit"
+    }
+}
+
+export default function Curve({children, backgroundColor}) {
+    const pathName = usePathname();
+
+    const [dimensions, setDimensions] = useState({
+        width: null,
+        height: null
+    })
+
+    useEffect( () => {
+        function resize(){
+            setDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+        resize();
+        window.addEventListener("resize", resize)
+        return () => {
+            window.removeEventListener("resize", resize);
+        }
+    }, [])
+    
+    return (
+    <div className='page curve' style={{backgroundColor}}>
+      {isLoading && <Preloader />}
+       <div style={{opacity: dimensions.width == null ? 1 : 0}} className='background'/>
+       <motion.p className='route' {...anim(text)}>
+            {paths[pathName]}
+        </motion.p>
+       {dimensions.width != null && <SVG {...dimensions}/>}
+        {
+            children
+        }
+    </div>
+    )
+}
+
+const SVG = ({height, width}) => {
+
+    const initialPath = `
+        M0 300 
+        Q${width/2} 0 ${width} 300
+        L${width} ${height + 300}
+        Q${width/2} ${height + 600} 0 ${height + 300}
+        L0 0
+    `
+
+    const targetPath = `
+        M0 300
+        Q${width/2} 0 ${width} 300
+        L${width} ${height}
+        Q${width/2} ${height} 0 ${height}
+        L0 0
+    `
+
+    return (
+        <motion.svg {...anim(translate)}>
+            <motion.path {...anim(curve(initialPath, targetPath))} />
+        </motion.svg>
+    )
+      }
