2b642eed29d6102212602508b343455a36f09721
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-20 02:11:31 +0100

    Update transitionProvider.jsx

--- src/app/components/transitionProvider.jsx
+++ src/app/components/transitionProvider.jsx
@@ -71,7 +71,7 @@ const TransitionProvider = ({ children }) => {
          
       <motion.div
          className='PageOut'
-         transition={{ delay: 1.8 }}
+         transition={{ delay: 2 }}
          >
         <motion.span
           className="PageOut1"
