83343ee5dcf4e7613c2e09f9a3f830292f636b56
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-22 11:54:58 +0100

    Update about3.js

--- src/app/components/about3.js
+++ src/app/components/about3.js
@@ -9,11 +9,7 @@ function About3() {
           <div className="container">
            <h5>Our Services</h5>
             <div className="services-list">
-              <motion.div
-               initial={{ opacity: 0, scale: 1.05 }}
-       whileInView={{ opacity: 1, scale: 1 }}
-      viewport={{ once: true }}
-       transition={{ duration: 0.6 }}>
+              <div>
                <i className="fa-thin fa-code"></i>
                 <h2>Web Development</h2>
                 <p>
@@ -27,13 +23,9 @@ function About3() {
                   We boast functonality, without the compromise of your
                   aesthetics.
                 </p>
-              </motion.div>
+    </div>
 
-              <motion.div
-              initial={{ opacity: 0, scale: 1.05 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-             viewport={{ once: true }}
-             transition={{ delay: 0.15, duration: 0.6 }}>
+              <div>
                 <i className="fa-brands fa-figma"></i>
                 <h2>UI/UX</h2>
                 <p>
@@ -48,13 +40,9 @@ function About3() {
                   Endless customization and creative innovations in design,
                   guaranteed to elevate your digital products.
                 </p>
-              </motion.div>
+                    </div>
 
-              <motion.div
-              initial={{ opacity: 0, scale: 1.05 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-             viewport={{ once: true }}
-             transition={{ delay: 0.3, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}>
+              <div>
                <i className="fa-thin fa-paintbrush"></i>
                 <h2>Graphic Design</h2>
                 <p>
@@ -69,13 +57,9 @@ function About3() {
                   <br />
                   Tailored specifically, <b>just for YOU!</b>
                 </p>
-              </motion.div>
+    </div>
 
-              <motion.div
-              initial={{ opacity: 0, scale: 1.05 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-             viewport={{ once: true }}
-             transition={{ delay: 0.45, duration: 0.6 }}>
+              <div>
                 <i className="fa-brands fa-app-store"></i>
                 <h2>App Design</h2>
                 <p>
@@ -92,7 +76,7 @@ function About3() {
                   <br />
                   seamlessly integrate you vision with top-tier algorithms.
                 </p>
-              </motion.div>
+                    </div>
             </div>
           </div>
         </div>
