e472f087e76778e0999088be5414066d4eb8437d
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-23 12:36:01 +0100

    Update craft1.js

--- src/app/components/craft1.js
+++ src/app/components/craft1.js
@@ -21,13 +21,21 @@ function Craft1() {
               Digital concepts given life
             </motion.h1>
 
-            <motion.h5
+          <motion.p
+        initial={{ opacity: 0, scale: 0 }}
+       whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+       transition={{ delay: 0.6, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}
+       >
+       Still confused?
+        </motion.p>
+       <motion.h5
                 initial={{ opacity: 0, scale: 0 }}
        whileInView={{ opacity: 1, scale: 1 }}
       viewport={{ once: true }}
        transition={{ delay: 0.6, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}
        >
-       Craft
+       See My Craft!
          </motion.h5>
          <div id="stacks">stacks</div>
          <div className="tab-titles">
