41443ab513ab896ea043b8e78020b0f0da4f048a
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-24 09:18:57 +0100

    Update about1.js

--- src/app/components/about1.js
+++ src/app/components/about1.js
@@ -12,13 +12,15 @@ function About1() {
         <motion.h5 initial={{ opacity: 0, scale: 0 }}
        whileInView={{ opacity: 1, scale: 1 }}
       viewport={{ once: true }}
-       transition={{ delay: 0.45, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}>What we're about?</motion.h5>
+       transition={{ delay: 0.45, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}>
+What we're About?
+    </motion.h5>
           <motion.h1
              initial={{ opacity: 0, scale: 0 }}
        whileInView={{ opacity: 1, scale: 1 }}
       viewport={{ once: true }}
        transition={{ delay: 0.3, duration: 0.6, ease: [0.16, 1, 0.3, 1] }}>
-            Creating symphonies of digital functionality and aesthetics
+            Digital functionality Meets Aesthetics
           </motion.h1>
           <h2>Meet The Mind Behind The Code</h2>
           <div className="row">
@@ -33,11 +35,11 @@ function About1() {
              <p className="circle-sketch-highlight">
           An award-winning, detail-oriented web | mobile designer & software engineer with expertise in <b>JavaScript</b>, <b>TypeScript</b>, <b>React</b>, <b>Vue.js</b>, <b>Motion design</b>, <b>Figma</b>, <b>flutter</b>, <b>React Native</b> and <b>Next.js</b> as my major frame-work.
             <br/> I continously strive to be the finest version of myself, my workflow is no exception. 
-            <br/> "As developers, we bridge the recess where tech meets standard" - Valhalla. 
-            <br/> A quote perfectly aligning with my goals as a software engineer. 
+                {/* <br/> "As developers, we bridge the recess where tech meets standard" - Valhalla. 
+            <br/> A quote perfectly aligning with my goals as a software engineer. */}
             <br/><br/>
-            I'd be ecstatic to chat over coffee ☕, reviewing your digital needs; Alternatively feel free to reach out and say Hello! 😉
-            <br/> I don't bite!;
+            I'd be ecstatic to chat over coffee ☕, reviewing your digital needs, or just to connect and say Hello!
+            <br/> I don't bite! :)
 
               </p>
                     
