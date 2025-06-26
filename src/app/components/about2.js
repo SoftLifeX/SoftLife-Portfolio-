dd326cb2d873d4bdca1c479972526ecfad7ac0e6
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-26 13:52:57 +0100

    Update about2.js

--- src/app/components/about2.js
+++ src/app/components/about2.js
@@ -7,7 +7,7 @@ function About2() {
   return (
     <div>
       <section className="about2">
-     <div id="stacks" />
+     <div id="stack"> stack </div>
         <div className="container">
          <div className="tab-titles">
                 <h4>
