f1b9c3c334b21861d9acfa8cc6bd68f820dec54b
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-01 22:51:44 +0100

    Update images.js

--- src/app/images/images.js
+++ src/app/images/images.js
@@ -1,12 +1,12 @@
 import counsel from './counsel.png'
-import upskill-africa from "./upskill-africa.png";
-import car-culture from "./car-culture.png";
+import upskill from "./upskill.png";
+import car from "./car.png";
 import jewlers from "./jewlers.png";
 
 
 export const images = {
   counsel,
-  upskill-africa,
-  car-culture,
+  upskill,
+  car,
   jewlers
 };
