8598dfbd8ac345fe50c7276252988978962fa0a7
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-01 11:59:35 +0100

    Update about2.js

--- src/app/components/about2.js
+++ src/app/components/about2.js
@@ -104,14 +104,6 @@ function About2() {
             <div className="h2boundary">
               <h2 className="marker-highlighter">Experience</h2>
             </div>
-              <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
-      <defs>
-      <filter id="marker-shape">
-      <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
-      <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-      </filter>
-      </defs>
-   </svg>
               <div className="tab-contents-exp">
                 <i className="fa-solid fa-laptop"></i>
                 <p>01/2023-Present</p>
