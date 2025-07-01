344236026810cf94136fb6914ee333bc28e980af
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-01 11:16:49 +0100

    Update about2.js

--- src/app/components/about2.js
+++ src/app/components/about2.js
@@ -37,15 +37,7 @@ function About2() {
           {action === "Tools" ? (
             
             <div className="tab-col-1">
-              <h2 className="marker-highlight">My Stack & Tools</h2>
-              <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
-      <defs>
-      <filter id="marker-shape">
-      <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
-      <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-      </filter>
-      </defs>
-   </svg>
+              <h2 className="marker-highlighter">My Stack & Tools</h2>
             <div className="tab-contents">
                 <ul>
                   <div className="tools">
@@ -88,15 +80,7 @@ function About2() {
                   </div>
                 </ul>
               </div>
-              <h2 className="marker-highlight">Certification & CV</h2>
-             <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
-      <defs>
-      <filter id="marker-shape">
-      <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
-      <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-      </filter>
-      </defs>
-   </svg>
+              <h2 className="marker-highlighter">Certification & CV</h2>
             <ul>
                 <div className="cert_cv">
                   <a href="./Daniel c. Daniel's Resume.pdf" download className="btn4">
@@ -113,15 +97,7 @@ function About2() {
             )}
             {action === "Experience" ? (
                         <div className="tab-col-2">
-              <h2 className="marker-highlight">Experience</h2>
-              <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
-      <defs>
-      <filter id="marker-shape">
-      <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
-      <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-      </filter>
-      </defs>
-   </svg>
+              <h2 className="marker-highlighter">Experience</h2>
               <div className="tab-contents-exp">
                 <i className="fa-solid fa-laptop"></i>
                 <p>01/2023-Present</p>
