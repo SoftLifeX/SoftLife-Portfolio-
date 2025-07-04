eb845d8f949889af2560d91f7fe02908b708108f
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-05 00:01:57 +0100

    Update contact.js

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -1,4 +1,3 @@
-'use client';
 import React, { useRef, useEffect } from "react";
 import Magnetic2 from "./magnetic2";
 import Lottie from "lottie-react";
@@ -13,43 +12,6 @@ import X from "@/app/assets/X.json";
 
 function Contact() {
 
-  const containerRef = useRef();
-  const [hasPlayed, setHasPlayed] = useState(false);
-
-  const options = {
-    animationData: X,
-    loop: false,
-    autoplay: false,
-  };
-
-  const { View, play, stop } = useLottie(options);
-
-  // Use native IntersectionObserver
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting && !hasPlayed) {
-          play();
-          setHasPlayed(true);
-        }
-      },
-      { threshold: 0.5 }
-    );
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    return () => {
-      if (containerRef.current) observer.unobserve(containerRef.current);
-    };
-  }, [hasPlayed, play]);
-
-  // Replay on hover
-  const handleHover = () => {
-    stop();
-    play();
-  };
 
   return (
     <div>
@@ -72,33 +34,33 @@ function Contact() {
           </div>
           <div className="socials">
                   <a href="https://x.com/SoftLife_Dev" target="_blank" data-title="X">
-                   <div
-                    ref={containerRef}
-                    onMouseEnter={handleHover}
-                    id="lottie3">{View}</div>
+                   <Lottie
+                  id="lottie3"
+                  loop={false} 
+                  animationData={X} />
                   </a>
                   <a href="https://wa.link/wjns9h" target="_blank" data-title="WhatsApp">
                     <Lottie
                   id="lottie3"
-                  loop={true} 
+                  loop={false} 
                   animationData={Whatsapp} />
                   </a>
                   <a href="https://www.instagram.com/softlifekid/" target="_blank" data-title="Instagram">
                     <Lottie
                   id="lottie3"
-                  loop={true} 
+                  loop={false} 
                   animationData={Instagram} />
                   </a>
                   <a href="https://linkedin.com/in/daniel-c-daniel-dev" target="_blank" data-title="linkedin">
                      <Lottie
                   id="lottie3"
-                  loop={true} 
+                  loop={false} 
                   animationData={LinkedIn} />
                   </a>
                   <a href="https://github.com/SoftLifeDev" target="_blank" data-title="Github">
                     <Lottie
                   id="lottie3"
-                  loop={true} 
+                  loop={false} 
                   animationData={Github} />
                   </a>
           </div>
