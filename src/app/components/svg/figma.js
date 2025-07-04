b5c4051632c8bb46db9ec1216cc26d6e88842f40
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-04 03:06:30 +0100

    Create figma.js

---
+++ src/app/components/svg/figma.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const Figma = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+  >
+    <path fill="#e64a19" d="M26 17h-8a7 7 0 1 1 0-14h8z"></path>
+    <path fill="#7c4dff" d="M25 31h-7a7 7 0 1 1 0-14h7z"></path>
+    <path fill="#66bb6a" d="M18 45a7 7 0 1 1 0-14h7v7a7 7 0 0 1-7 7"></path>
+    <path fill="#ff7043" d="M32 17h-7V3h7a7 7 0 1 1 0 14"></path>
+    <circle cx="32" cy="24" r="7" fill="#29b6f6"></circle>
+  </svg>
+);
+
+export default Figma;
+      
