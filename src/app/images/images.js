df25851f3d1d6e18d25f482f9621defd78f4a767
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-04 01:27:26 +0100

    Update images.js

--- src/app/images/images.js
+++ src/app/images/images.js
@@ -2,6 +2,23 @@ import counsel from './counsel.jpg'
 import upskill from "./upskill.png";
 import car from "./car.png";
 import jewlers from "./jewlers.png";
+import html from "./html.svg";
+import bootstrap from "./bootstrap.svg";
+import css from "./css.svg";
+import tailwind from "./tailwind.svg";
+import sass from "./sass.svg";
+import vue from "./vue.svg";
+import express from "./express.svg";
+import javascript from "./javascript.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import framer from "./framer.svg";
+import gitlab from "./gitlab.svg";
+import nextjs from "./nextjs.svg";
+import git from "./git.svg";
+import bitbucket from "./bitbucket.svg";
+import figma from "./figma.svg";
+import typescript from "./typescript.svg";
 
 
 export const images = {
