c8d9d464f09a8fa3ebf7b26db2df93e802ed84db
SoftLifeDev <softlife.dev001@gmail.com>
2025-07-04 03:00:34 +0100

    Create framer.js

---
+++ src/app/components/svg/framer.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+const Framer = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    baseProfile="basic"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M5.811 1H19a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-7L5.28 2.28A.75.75 0 0 1 5.811 1"
+      opacity="0.35"
+    ></path>
+    <path d="M5 9h7l6.72 6.72a.75.75 0 0 1-.53 1.28H5a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1"></path>
+    <path
+      d="M4 10v6.006a2.4 2.4 0 0 0 .703 1.697l6.017 6.017a.75.75 0 0 0 1.28-.53V9H5a1 1 0 0 0-1 1"
+      opacity="0.35"
+    ></path>
+  </svg>
+);
+
+export default Framer;
+      
