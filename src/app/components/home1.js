4ccacc547729b54deb28e0569bdf78020c480476
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-08 22:00:31 +0100

    Update home1.js

--- src/app/components/home1.js
+++ src/app/components/home1.js
@@ -121,7 +121,7 @@ function Home1() {
                 <span>
                  <ScrambleText 
                 text={" I'm Daniel c. Daniel.\nSoftware Engineer."}
-                speed={3} 
+                speed={4} 
                 autoLineDelay={true} />
                 </span>
 {/* <ReplaceText
