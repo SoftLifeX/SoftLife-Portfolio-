0e607204520962c78f8fc7706ea31b0b871bf55b
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-08 07:52:36 +0100

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,5 @@
-"use client";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import React from "react";
-import { useEffect } from "react";
+import Navigation from "../components/navigation";
 import About1 from "../components/about1";
 import About2 from "../components/about2";
 import About3 from "../components/about3";
@@ -11,32 +7,14 @@ import Contact from "../components/contact";
 
 const About = () => {
 
-  const pathName = usePathname();
-
-      useEffect(() => {
-        (async () => {
-          const LocomotiveScroll = (await import("locomotive-scroll")).default;
-  
-          const locomotiveScroll = new LocomotiveScroll();
-        })();
-      }, []);
-
   return (
     
-      <div>
-      <svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters">
-      <defs>
-      <filter id="marker-shape">
-      <feTurbulence type="fractalNoise" baseFrequency="0 0.15" numOctaves="1" result="warp" />
-      <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-      </filter>
-      </defs>
-      </svg>
+      <Navigation>
       <About1 />
       <About2 />
       <About3 />
       <Contact />
-    </div>
+    </Navigation>
    
   );
 };
