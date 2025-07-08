807246169841ef0ab5840126b5a02a25345aeaf6
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-08 07:46:21 +0100

    Update navigation.js

--- src/app/components/navigation.js
+++ src/app/components/navigation.js
@@ -1,5 +1,27 @@
-'use client'
+'use client';
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import React from "react"
+
+export default function Navigation = () => {
+
+  const pathName = usePathname();
+
+      useEffect(() => {
+        (async () => {
+          const LocomotiveScroll = (await import("locomotive-scroll")).default;
+  
+          const locomotiveScroll = new LocomotiveScroll();
+        })();
+      }, []);
+
+  return (
+    
+   <div>
+    {children}
+   </div>
+   
+  );
+};
+    
 
