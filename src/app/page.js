3d90261b03bd90f5658b8a75bbb39db172a92960
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-08 08:22:04 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,34 @@
-import React from "react"
-import Navigation from "../components/navigation";
+"use client";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { useEffect } from "react";
 import Home1 from "./components/home1";
 import Home2 from "./components/home2";
 import Contact from "./components/contact";
 
 
 const Home = () => {
-   
+   const pathName = usePathname();
+     
+     useEffect(() => {
+          (async () => {
+            const LocomotiveScroll = (await import("locomotive-scroll")).default;
+    
+            const locomotiveScroll = new LocomotiveScroll();
+          })();
+        }, []);
 
   return (
-    <Navigation>
+    <div>
 
       
       <Home1 />
       <Home2 />
       <Contact />
 
-     </Navigation>
+     </div>
   );
 };
 
