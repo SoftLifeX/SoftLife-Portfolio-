1d253dbf4b1f32041b46953c4de1d9af1767691f
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-08 07:53:21 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Navigation from "../components/navigation";
 import Home1 from "./components/home1";
 import Home2 from "./components/home2";
 import Contact from "./components/contact";
@@ -8,14 +9,14 @@ const Home = () => {
    
 
   return (
-    <div>
+    <Navigation>
 
       
       <Home1 />
       <Home2 />
       <Contact />
 
-     </div>
+     </Navigation>
   );
 };
 
