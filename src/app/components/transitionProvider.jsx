55b8dac7f65bf3d46f72a5dd1344a6ca03cb248e
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-11 13:39:31 +0100

    Update transitionProvider.jsx

--- src/app/components/transitionProvider.jsx
+++ src/app/components/transitionProvider.jsx
@@ -3,7 +3,7 @@
 import { AnimatePresence } from "framer-motion";
 import Header from "./header";
 import Contact from "./contact";
-import Revealer from "./Revealer";
+import Revealer from "./revealer";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
