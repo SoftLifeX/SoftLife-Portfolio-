3d71b2959f97835936973d4c63f30cca1634a3d7
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-11 07:36:49 +0100

    Update useRevealer.js

--- src/app/components/hooks/useRevealer.js
+++ src/app/components/hooks/useRevealer.js
@@ -2,3 +2,7 @@
 import gsap from 'gsap';
 import {useGSAP} from 'gsap/react';
 import CustomEase from 'gsap/CustomEase';
+
+
+gsap.registerPlugin(CustomEase);
+    CustomEase.create("hop", "0.9, 0, 0.1, 1"):
