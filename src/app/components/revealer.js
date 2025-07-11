e39be6c195e4a3acaee89f4046811f62f3a8dac6
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-11 16:08:02 +0100

    Update revealer.js

--- src/app/components/revealer.js
+++ src/app/components/revealer.js
@@ -1,16 +1,23 @@
 'use client'
 import { useState, useRef, useEffect } from 'react';
 import gsap from 'gsap';
-
+import useGSAP from 
 
 
 
 function Revealer() {
    const revealer = useRef(null);
 
+   useGSAP(() => {
+    gsap.to(revealer.current, {
+       scaleY: 0,
+       duration: 1.25,
+       delay: 1,
+    });
+  }, []);
+
     
     useEffect( () => {
-    gsap.registerPlugin(CustomEase);
     gsap.to(revealer.current, {
       scaleY: 0,
       duration: 1.25,
