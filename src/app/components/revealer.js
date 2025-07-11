3545bced1a68a4bff487037ef6acd3c615f75298
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-11 13:46:00 +0100

    Update revealer.js

--- src/app/components/revealer.js
+++ src/app/components/revealer.js
@@ -13,12 +13,10 @@ function Revealer() {
     
     useEffect( () => {
     gsap.registerPlugin(CustomEase);
-   CustomEase.create("hop", "0.9, 0, 0.1, 1"):
     gsap.to(revealer, {
       scaleY: 0,
       duration: 1.25,
       delay: 1,
-      ease: "hop",
     });
     }, []);
 
