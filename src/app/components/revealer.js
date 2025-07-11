de0ab7304f0e7d1c17ae498e7280b67c727590c5
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-11 13:33:43 +0100

    Update revealer.js

--- src/app/components/revealer.js
+++ src/app/components/revealer.js
@@ -4,19 +4,23 @@ import gsap from 'gsap';
 import {useGSAP} from 'gsap/react';
 import CustomEase from 'gsap/CustomEase';
 
-const revealer = useRef(null);
-gsap.registerPlugin(CustomEase);
-CustomEase.create("hop", "0.9, 0, 0.1, 1"):
+
+
 
 function Revealer() {
-    useGSAP(() => {
-    gsap.to( ".revealer", {
+   const revealer = useRef(null);
+
+    
+    useEffect( () => {
+    gsap.registerPlugin(CustomEase);
+   CustomEase.create("hop", "0.9, 0, 0.1, 1"):
+    gsap.to(revealer, {
       scaleY: 0,
       duration: 1.25,
       delay: 1,
       ease: "hop",
     });
-    }, {});
+    }, []);
 
 return (
     <div ref={revealer} className="revealer" />
