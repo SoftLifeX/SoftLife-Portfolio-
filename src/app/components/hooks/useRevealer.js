9f91308fb079d0a9ac9391aded7335d80dcaa359
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-12 05:24:11 +0100

    Update useRevealer.js

--- src/app/components/hooks/useRevealer.js
+++ src/app/components/hooks/useRevealer.js
@@ -1,21 +1,31 @@
 'use client'
+import { useState, useRef, useEffect } from 'react';
 import gsap from 'gsap';
-import {useGSAP} from 'gsap/react';
+import {useGSAP} from '@gsap/react';
 import CustomEase from 'gsap/CustomEase';
 
 
-gsap.registerPlugin(CustomEase);
-CustomEase.create("hop", "0.9, 0, 0.1, 1"):
 
 function useRevealer() {
-    useGSAP(() => {
-    gsap.to( ".revealer", {
-      scaleY: 0,
-      duration: 1.25,
-      delay: 1,
-      ease: "hop",
+
+   gsap.registerPlugin(CustomEase);
+
+   const ease = CustomEase.create("hop", "0.9, 0, 0.1, 1");
+
+   useGSAP(() => {
+      
+    gsap.to(".revealer", {
+       scaleY: 0,
+       duration: 1.25,
+       delay: 1,
+       ease: ease,
     });
-    }, {});
+  }, []);
+
+
+return (
+    <div ref={revealer} className="revealer" />
+ );
 }
 
 export default useRevealer
