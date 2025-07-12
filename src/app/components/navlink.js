506a15c57b8c16cda0ab08e15621b2bbf470f786
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-12 07:37:47 +0100

    Update navlink.js

--- src/app/components/navlink.js
+++ src/app/components/navlink.js
@@ -9,6 +9,31 @@ import { usePathname } from 'next/navigation'
 
 const NavLink = ({ link }) => {
 
+  const pathName = usePathname();
+  const router = useTransitionRouter();
+  function triggerPageTransition() {
+  document.documentElement.animate([
+    {
+      clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)"
+    },
+    {
+      clipPath: "polygon(25% 75%, 75% 75%, 75% 75%, 25% 75%)"
+    },
+  ], {
+    duration: 2000,
+    easing: 'cubic-bezier(0.9, 0, 0.1, 1)',
+    pseudoElement: '::view-transition-new(root)'
+  });
+  }
+  const handleNavigation = (path) => (e) => {
+  if (path === pathName) {
+    e.preventDefault();
+    return;
+  }
+  router.push(path, {
+    onTransitionReady: triggerPageTransition,
+  });
+};
   
   return (
     <Link onClick={handleNavigation(link.url)} className={pathName === link.url ? "active" : ""} href={link.url}>
