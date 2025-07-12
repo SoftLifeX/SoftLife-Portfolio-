0eca4fa26368e36a583dd24e42a4a9c6bc62338c
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-12 05:40:26 +0100

    Update transitionProvider.jsx

--- src/app/components/transitionProvider.jsx
+++ src/app/components/transitionProvider.jsx
@@ -6,31 +6,14 @@ import Contact from "./contact";
 import useRevealer from "./hooks/useRevealer";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-import { useTransitionRouter } from 'next-view-transitions'
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Preloader from "../Preloader"
 import StickyCursor from "../components/stickyCursor"
 
 
-  const router = useTransitionRouter();
-
-  export function triggerPageTransition() {
-  document.documentElement.animate([
-    {
-      clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)"
-    },
-    {
-      clipPath: "polygon(25% 75%, 75% 75%, 75% 75%, 25% 75%)"
-    },
-  ], {
-    duration: 2000,
-    easing: 'cubic-bezier(0.9, 0, 0.1, 1)',
-    pseudoElement: '::view-transition-new(root)'
-  });
-  }
-
 
 const TransitionProvider = ({ children }) => {
+  
   useRevealer();
 
    const [isLoading, setIsLoading] = useState(true);
