64671cb87366bd5e1e57c63a9d1db67b3d6b53d0
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-13 08:40:22 +0100

    Update overlay.js

--- src/app/components/overlay.js
+++ src/app/components/overlay.js
@@ -43,7 +43,7 @@ export default function Overlay() {
         position: "fixed",
         top: 0, left: 0, right: 0, bottom: 0,
         zIndex: 9999,
-        background: "black",
+        background: "transparent",
         pointerEvents: "none",
         display: "flex",
         alignItems: "center",
