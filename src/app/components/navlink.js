2a7a7fd6adecd2c633229f0ac3ec011f4e1b369e
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-13 10:15:16 +0100

    Update navlink.js

--- src/app/components/navlink.js
+++ src/app/components/navlink.js
@@ -12,6 +12,21 @@ const NavLink = ({ link }) => {
   const pathName = usePathname();
   const router = useTransitionRouter();
 
+  function TriggerPageTransition () {
+    document.documentElement.animate([
+      {
+        clipPath: "polygon(25% 75% 75% 75% 75% 75% 25% 75%)"
+      }
+      {
+        clipPath: "polygon(0% 100% 100% 100% 100% 0% 0% 0%)"
+      }
+    ],{
+      duration: 2000,
+      easing: "cubic-bezier(0.9, 0, 0.1, 1)",
+      pseudoElement: "::view-transition-new(root)",
+    });
+  }
+
   
   
   return (
