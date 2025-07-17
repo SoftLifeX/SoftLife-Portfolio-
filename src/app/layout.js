d57b94895f915f0e52bb945f411a8b4ea20071bb
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-17 05:57:47 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,48 +81,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-    <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css"
-      />
-
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-solid.css"
-      />
-
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-regular.css"
-      />
-
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/sharp-light.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/duotone.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://site-assets.fontawesome.com/releases/v6.7.2/css/brands.css"
-      />
-        <link
-          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-          rel="stylesheet"
-        />
-        <script
-          src="https://kit.fontawesome.com/2f82fb206f.js"
-          crossOrigin="anonymous"
-        ></script>
         <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
 
         <script src="https://code.jquery.com/pep/0.4.3/pep.js"></script>
-        <script
-          src="https://kit.fontawesome.com/2f82fb206f.js"
-          crossOrigin="anonymous"
-        ></script>
+        
         <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
