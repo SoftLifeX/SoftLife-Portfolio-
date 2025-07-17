e3c855c1340fa242d7e78c3f660f86597a6f8e9b
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-17 11:32:51 +0100

    Update providers.jsx

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
+import TransitionProvider from "./components/transitionProvider";
 
 export function Providers({ children }) {
-    return 
+    return (
 <ThemeProvider defaultTheme='system' enableSystem>
+ <TransitionProvider>
   {children}
+ </TransitionProvider>
 </ThemeProvider>
+ )
 }
