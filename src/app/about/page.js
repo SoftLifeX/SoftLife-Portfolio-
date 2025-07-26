92d36b08274c1266e45762fa418f68cc5c98a16d
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-26 10:45:30 +0100

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,9 +7,11 @@ import { useEffect } from "react";
 import About1 from "../components/about1";
 import Contact from "../components/contact";
 import About3 from "../components/about3";
+import { useRevealer } from "../components/hooks/useRevealer";
 
 const About = () => {
 const pathName = usePathname();
+useRevealer();
      
      useEffect(() => {
           (async () => {
@@ -21,6 +23,7 @@ const pathName = usePathname();
   return (
     
     <div>
+      <div className="revealer" />
       <About1 />
       <About3 />
        <Contact />
