debc24217351d487ad51902054cd9d92d8b2e948
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-26 20:50:44 +0100

    Create transitionlink.js

---
+++ src/app/components/transitionlink.js
@@ -0,0 +1,83 @@
+'use client'
+
+import React from 'react'
+
+import Link from 'next/link'
+
+import { usePathname } from 'next/navigation'
+
+import { useTransitionRouter } from "next-view-transitions";
+
+
+
+
+
+const pageAnimation = () => {
+
+  document.documentElement.animate(
+
+    [
+
+      {
+
+        clipPath: "polygon(25% 75%, 75% 75%, 75% 75%, 25% 75%)",
+
+      },
+
+      {
+
+        clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+
+      },
+
+    ],
+
+    {
+
+      duration: 2000,
+
+      easing: "cubic-bezier(0.9, 0, 0.1, 1)",
+
+      pseudoElement: "::view-transition-new(root)",
+
+    }
+
+  );
+
+}
+
+
+
+const TransitionLink = () => {
+
+
+const pathname = usePathname();
+  const router = useTransitionRouter();
+  
+  const handleNav = (path) => (e) => {
+ if (path === pathname) {
+  e.preventDefault(); 
+  return;
+ }
+  
+  router.push(path, {
+  onTransitionReady: pageAnimation,
+ });
+};
+
+
+
+
+  return (
+
+  <Link href={href} onClick={handleNav}>
+    {label}
+  </Link>
+
+  );
+
+};
+
+
+
+export default TransitionLink
