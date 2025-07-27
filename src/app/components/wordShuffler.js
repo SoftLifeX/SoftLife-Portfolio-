01e6200cbc710fc40cb218b83586090a63ac1a72
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-27 01:23:14 +0100

    Create wordShuffler.js

---
+++ src/app/components/wordShuffler.js
@@ -0,0 +1,51 @@
+'use client';
+
+import { useRef, useState, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { useGsap } from './hooks/useGsap'; 
+
+const words = ['Content Creator', 'Travel Enthusiast', 'a bit of a gamer', 'Lover of the Arts'];
+
+export default function WordShuffler() {
+  const wordRef = useRef(null);
+  const [index, setIndex] = useState(0);
+
+  // Animate the word change
+  useGsap(wordRef, (el) => {
+    const tl = gsap.timeline();
+    tl.to(el, { y: '100%', duration: 0.4, ease: 'power2.in' });
+    tl.set(el, { y: '-100%' });
+    tl.to(el, { y: '0%', duration: 0.4, ease: 'power2.out' });
+    return tl;
+  }, [index]);
+
+  // Trigger new word every 2.5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % words.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      style={{
+        overflow: 'hidden',
+        height: '1.5em',
+        display: 'inline-block',
+        position: 'relative',
+      }}
+    >
+      <div
+        ref={wordRef}
+        style={{
+          display: 'inline-block',
+          transform: 'translateY(0%)',
+          willChange: 'transform',
+        }}
+      >
+        {words[index]}
+      </div>
+    </div>
+  );
+}
