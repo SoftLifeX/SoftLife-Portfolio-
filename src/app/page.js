672af32661a336042a1362215c8df6cc52773497
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-28 21:47:35 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,25 @@ import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
 import React from "react";
 import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 import Home1 from "./components/home1";
 import Home2 from "./components/home2";
 import About2 from "./components/about2";
 import Contact from "./components/contact";
 import { useRevealer } from "./components/hooks/useRevealer";
 
-
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 const Home = () => {
    useRevealer();
      
-     useEffect(() => {
-          (async () => {
-            const LocomotiveScroll = (await import("locomotive-scroll")).default;
-    
-            const locomotiveScroll = new LocomotiveScroll();
-          })();
-        }, []);
+     useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  }, []);
 
   return (
     <div>
