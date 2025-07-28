03c1797de8b5396d50ae392266250916c4d7ea92
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-28 22:52:42 +0100

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,29 +5,34 @@ import Link from "next/link";
 import { motion, useInView, AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 import Contactform from "../components/contactform";
 import { useRevealer } from "../components/hooks/useRevealer";
 
-
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 const Contact = () => {
 
 useRevealer();
 
 const pathName = usePathname();
 
-     useEffect(() => {
-          (async () => {
-            const LocomotiveScroll = (await import("locomotive-scroll")).default;
-    
-            const locomotiveScroll = new LocomotiveScroll();
-          })();
-        }, []);
+useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+      smoothTouch: 0.1,
+    });
+  });
 
 return(
-    <div>
+   <div id="smooth-wrapper">
+    <div id="smooth-content">
      <div className="revealer" />
      <Contactform />
     </div>
+  </div>
   );
 };
 
