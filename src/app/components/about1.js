300514fc0d605f67043a53143699b8a9bd84b1b2
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-31 15:01:07 +0100

    Update about1.js

--- src/app/components/about1.js
+++ src/app/components/about1.js
@@ -39,6 +39,10 @@ function About1() {
   gsap.set(".title", { opacity: 1 });
   }, []);
 
+document.fonts.ready.then(() => {
+  gsap.set(".subtitle", { opacity: 1 });
+  }, []);
+   
    const heroSplit = new SplitText(".title", {
          type: "chars, words, lines",
          mask: "lines",
@@ -55,6 +59,10 @@ function About1() {
       });
     }
   });
+
+
+
+
 
    const subSplit = new SplitText(".subtitle", {
          type: "chars, words, lines",
