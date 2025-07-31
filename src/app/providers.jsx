ec93b04c1527335cb906c03381013c79aae8adae
Daniel Daniel c <softlife.dev001@gmail.com>
2025-07-31 14:38:35 +0100

    Update providers.jsx

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,7 +1,5 @@
 'use client'
-
 import { ThemeProvider } from 'next-themes'
-import { AnimatePresence } from "framer-motion";
 import Header from "./components/header";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
@@ -15,7 +13,6 @@ import CustomCursor from "./components/CustomCursor"
 
 export function Providers({ children }) {
 
-const [isLoading, setIsLoading] = useState(true);
 
   const paths = {
 
@@ -32,16 +29,6 @@ const [isLoading, setIsLoading] = useState(true);
 
   const pathName = usePathname();
 
-
-    useEffect(() =>{
-      setTimeout(() => {
-             setIsLoading(false);
-
-             document.body.style.cursor = "default";
-
-             window.scrollTo(0, 0);
-           }, 2000);
-          },[]);
 
     return (
 <ThemeProvider defaultTheme='system' enableSystem>
