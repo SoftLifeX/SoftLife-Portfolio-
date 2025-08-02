2b2fe8bb172a4411da90c40b1f89f1b0ffbda15e
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-02 16:13:41 +0100

    Update images.js

--- src/app/images/images.js
+++ src/app/images/images.js
@@ -2,28 +2,24 @@ import counsel from './counsel.jpg'
 import upskill from "./upskill.png";
 import car from "./car.png";
 import jewlers from "./jewlers.png";
-import html from "./html.svg";
-import bootstrap from "./bootstrap.svg";
-import css from "./css.svg";
-import tailwind from "./tailwind.svg";
-import sass from "./sass.svg";
-import vue from "./vue.svg";
-import express from "./express.svg";
-import javascript from "./javascript.svg";
-import nodejs from "./nodejs.svg";
-import react from "./react.svg";
-import framer from "./framer.svg";
-import gitlab from "./gitlab.svg";
-import nextjs from "./nextjs.svg";
-import git from "./git.svg";
-import bitbucket from "./bitbucket.svg";
-import figma from "./figma.svg";
-import typescript from "./typescript.svg";
+import metaverse from './metaverse.png';
+import github from './github.png';
+import ochi from './ochi.png';
+import supadupa from "./supadupa.png";
+import lightsass from "./lightsass.png";
+import flow from "./flow.png";
 
 
 export const images = {
   counsel,
   upskill,
   car,
-  jewlers
+  jewlers,
+  flow,
+  lightsass,
+  supadupa,
+  ochi,
+  github,
+  metaverse,
 };
+
