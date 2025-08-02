d9b21345343297a411d25fce5c997734f417d921
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-02 01:25:11 +0100

    Create smoother.js

---
+++ src/app/components/smoother.js
@@ -0,0 +1,19 @@
+import { gsap } from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+
+let smoother;
+
+export function initSmoother() {
+  if (!smoother) {
+    gsap.registerPlugin(ScrollSmoother);
+    smoother = ScrollSmoother.create({
+      smooth: 1,
+      effects: true,
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+    });
+  }
+  return smoother;
+}
+
+export { smoother };
