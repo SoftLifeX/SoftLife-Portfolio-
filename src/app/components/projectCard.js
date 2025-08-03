d533a8d162c52d1f38e89a1aaa54f66008795087
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-03 04:33:06 +0100

    Update projectCard.js

--- src/app/components/projectCard.js
+++ src/app/components/projectCard.js
@@ -1,16 +1,19 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function ProjectCard({ item, index, setModal }) {
   return (
     <div
-      className="card-wrapper"
+      className="project-card"
       onMouseEnter={() => setModal({ active: true, index })}
       onMouseLeave={() => setModal({ active: false, index })}
     >
-      <Link href={item.href} target="_blank" className="card-link">
-        <Image src={item.img} alt={`${item.title} Image`} className="card-img" />
+      <Link href={item.href} target="_blank" className="image-wrapper">
+        <Image
+          src={item.img}
+          alt={`${item.title} Image`}
+          className="image"
+        />
       </Link>
     </div>
   );
