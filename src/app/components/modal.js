807bc175b5fe9f23a0fcc372ded39b28e8209874
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-03 03:41:12 +0100

    Update modal.js

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from "react";
+"use client";
+import { useEffect, useRef } from "react";
+import Image from "next/image";
 import { motion } from "framer-motion";
 import gsap from "gsap";
-import { scaleAnimation } from "./scaleAnimation";
-
+import { scaleAnimation } from "../animations/scaleAnimation";
 
 export default function Modal({ modal, projects }) {
   const { active, index } = modal;
@@ -11,40 +12,80 @@ export default function Modal({ modal, projects }) {
   const cursorLabel = useRef(null);
 
   useEffect(() => {
-    const xMoveContainer = gsap.quickTo(modalContainer.current, "left", { duration: 0.8, ease: "power3" });
-    const yMoveContainer = gsap.quickTo(modalContainer.current, "top", { duration: 0.8, ease: "power3" });
-    const xMoveCursor = gsap.quickTo(cursor.current, "left", { duration: 0.5, ease: "power3" });
-    const yMoveCursor = gsap.quickTo(cursor.current, "top", { duration: 0.5, ease: "power3" });
-    const xMoveLabel = gsap.quickTo(cursorLabel.current, "left", { duration: 0.45, ease: "power3" });
-    const yMoveLabel = gsap.quickTo(cursorLabel.current, "top", { duration: 0.45, ease: "power3" });
+    const xMoveContainer = gsap.quickTo(modalContainer.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    const yMoveContainer = gsap.quickTo(modalContainer.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
 
-    const moveHandler = (e) => {
+    const xMoveCursor = gsap.quickTo(cursor.current, "left", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    const yMoveCursor = gsap.quickTo(cursor.current, "top", {
+      duration: 0.5,
+      ease: "power3",
+    });
+
+    const xMoveCursorLabel = gsap.quickTo(cursorLabel.current, "left", {
+      duration: 0.45,
+      ease: "power3",
+    });
+    const yMoveCursorLabel = gsap.quickTo(cursorLabel.current, "top", {
+      duration: 0.45,
+      ease: "power3",
+    });
+
+    const handleMouseMove = (e) => {
       const { pageX, pageY } = e;
       xMoveContainer(pageX);
       yMoveContainer(pageY);
       xMoveCursor(pageX);
       yMoveCursor(pageY);
-      xMoveLabel(pageX);
-      yMoveLabel(pageY);
+      xMoveCursorLabel(pageX);
+      yMoveCursorLabel(pageY);
     };
 
-    window.addEventListener("mousemove", moveHandler);
-    return () => window.removeEventListener("mousemove", moveHandler);
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
   }, []);
 
   return (
     <>
-      <motion.div ref={modalContainer} className="modal-container" variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}>
-        <div className="modal-slider" style={{ top: `${index * -100}%` }}>
-          {projects.map((project, i) => (
-            <div className="modal-slide" style={{ backgroundColor: project.color }} key={`modal_${i}`}>
-              <img src={project.img} alt="project" className="modal-img" />
+      <motion.div
+        ref={modalContainer}
+        variants={scaleAnimation}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+        className="modal-container"
+      >
+        <div style={{ top: index * -100 + "%" }} className="modal-slider">
+          {projects.map((project, idx) => (
+            <div className="modal-slide" style={{ backgroundColor: project.color }} key={idx}>
+              <Image src={project.img} alt="image" width={300} height={300} className="modal-img" />
             </div>
           ))}
         </div>
       </motion.div>
-      <motion.div ref={cursor} className="modal-cursor" variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"} />
-      <motion.div ref={cursorLabel} className="modal-cursor" variants={scaleAnimation} initial="initial" animate={active ? "enter" : "closed"}>
+
+      <motion.div
+        ref={cursor}
+        className="modal-cursor"
+        variants={scaleAnimation}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+      />
+
+      <motion.div
+        ref={cursorLabel}
+        className="modal-cursor"
+        variants={scaleAnimation}
+        initial="initial"
+        animate={active ? "enter" : "closed"}
+      >
         View
       </motion.div>
     </>
