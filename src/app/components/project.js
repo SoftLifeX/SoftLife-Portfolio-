509fc47dacd85b8e02b0db6146bf2c4ae62d441e
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-03 21:57:14 +0100

    Update project.js

--- src/app/components/project.js
+++ src/app/components/project.js
@@ -1,10 +1,55 @@
 import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { project } from "./constants";
 import ProjectCard from "./projectCard";
 import Modal from "./modal";
 
 export default function Project() {
+
+  //splitText
+  useGSAP(() => {
+  document.fonts.ready.then(() => {
+    // Initial reveal for both
+    gsap.set([".title"], { opacity: 1, lineHeight: "0.8em" });
+
+    // === Title Animation ===
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words, lines",
+      mask: "lines",
+      linesClass: "lineParent",
+      charsClass: "char-inner",
+      autoSplit: true,
+      onSplit: (self) => {
+        const stagger = 0.025;
+        const delay = 1.6;
+        const duration = 0.4;
+        const totalDuration = self.chars.length * stagger;
+
+        // Animate chars
+        gsap.from(self.chars, {
+          opacity: 0,
+          yPercent: 40,
+          duration,
+          ease: "back",
+          stagger,
+          delay,
+        });
+
+        // Animate line-height globally in sync
+        gsap.to(".title", {
+          lineHeight: "1.2em",
+          duration: totalDuration,
+          ease: "power1.out",
+          delay,
+        });
+      },
+    });
+  });
+}, []);
+  
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
@@ -23,8 +68,8 @@ export default function Project() {
   const [modal, setModal] = useState({ active: false, index: 0 });
 
   return (
-    <section style={{ margin: "100px 0" }} id="projects">
-      <h1>
+    <section style={{ margin: "100px 0" }} className="projects">
+      <h1 className="title">
         Selected  Projects
       </h1>
 
