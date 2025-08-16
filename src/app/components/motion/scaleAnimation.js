79f25d161d1e36bf6a6bfb755c69ee6799cdf3bc
Daniel Daniel c <softlife.dev001@gmail.com>
2025-08-16 08:18:51 +0100

    Update scaleAnimation.js

--- src/app/components/motion/scaleAnimation.js
+++ src/app/components/motion/scaleAnimation.js
@@ -1,13 +1,15 @@
 export const scaleAnimation = {
-  initial: { scale: 0, opacity: 0 },
+  initial: { scale: 0, x: "-50%", y: "-50%" },
   enter: {
     scale: 1,
-    opacity: 1,
+    x: "-50%",
+    y: "-50%",
     transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
   },
   closed: {
     scale: 0,
-    opacity: 0,
+    x: "-50%",
+    y: "-50%",
     transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
   },
 };
